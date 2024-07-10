d1247c4ecf4e4adce55bf20cdbacd6ac0efefbe7
maskpp <maskpp266@gmail.com>
2024-07-10 10:24:45 +0800

    add variable to control EndSuite

--- hivesim/hive.go
+++ hivesim/hive.go
@@ -25,6 +25,8 @@ type Simulation struct {
 	m    testMatcher
 	docs *docsCollector
 	ll   int
+
+	enableEndSuite bool
 }
 
 // New looks up the hive host URI using the HIVE_SIMULATOR environment variable
@@ -119,8 +121,15 @@ func (sim *Simulation) StartSuite(suite *simapi.TestRequest, simlog string) (Sui
 	return resp, err
 }
 
+func (sim *Simulation) EnableEndSuite() {
+	sim.enableEndSuite = true
+}
+
 // EndSuite signals the end of a test suite.
 func (sim *Simulation) EndSuite(testSuite SuiteID) error {
+	if !sim.enableEndSuite {
+		return nil
+	}
 	if sim.docs != nil {
 		return sim.docs.EndSuite(testSuite)
 	}
