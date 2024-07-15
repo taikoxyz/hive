2728e40e79e9de857ff4d708a448102f80956ce6
maskpp <maskpp266@gmail.com>
2024-07-15 17:11:25 +0800

    add bindings and relate scripts

---
+++ taiko/scripts/abigen.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ ! -d "taiko-mono/packages/protocol/out" ]; then
+    echo "ABI not generated in protocol package yet. Please run npm install && npx hardhat compile in ../protocol"
+    exit 1
+fi
+
+paths=("TaikoToken.sol")
+
+names=("TaikoToken")
+
+for (( i = 0; i < ${#paths[@]}; ++i ));
+do
+    lower=$(echo "${names[i]}" | tr '[:upper:]' '[:lower:]')
+    mkdir -p bindings/$lower
+    jq .abi taiko-mono/packages/protocol/out/${paths[i]}/${names[i]}.json > bindings/$lower/${names[i]}.json
+    abigen --abi bindings/$lower/${names[i]}.json \
+    --pkg $lower \
+    --type ${names[i]} \
+    --out bindings/$lower/${names[i]}.go
+done
+
+exit 0
---
+++ taiko/scripts/get_txs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm l1contract_txs.txt /dev/null 2>&1
+
+L1_NODE_HTTP_ENDPOINT=${L1_NODE_HTTP_ENDPOINT:-http://localhost:8545}
+START_BLOCK=0
+END_BLOCK=$(cast block-number --rpc-url "$L1_NODE_HTTP_ENDPOINT")
+
+for ((block = $START_BLOCK; block <= $END_BLOCK; block++)); do
+  echo "Checking block $block"
+  # Get all the transactions.
+  transactions=$(cast block $block --json --rpc-url "$L1_NODE_HTTP_ENDPOINT" | jq -r '.transactions[]')
+
+  # Store txs.
+  for tx in $transactions; do
+    echo "tx hash: $tx"
+    if [ "$tx" != "" ]; then
+      cast tx "$tx" --json --rpc-url "$L1_NODE_HTTP_ENDPOINT" >>l1contract_txs.txt
+    fi
+  done
+done
