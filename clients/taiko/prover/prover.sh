72d9c4a74d1c1949a49809b54212a628398b0d14
maskpp <maskpp266@gmail.com>
2024-07-16 23:32:10 +0800

    update docker files

--- clients/taiko/prover/prover.sh
+++ clients/taiko/prover/prover.sh
@@ -2,6 +2,12 @@
 
 . /taiko-client/common.sh
 
+export PROVER_DUMMY=true
+export PROVER_CAPACITY=1024
+export PROVER_L1_NODE_VERSION=1.0.0
+export PROVER_L2_NODE_VERSION=0.1.0
+export PROVER_ALLOWANCE=10.0
+
 # default env
 export JWT_SECRET="/taiko-client/jwt.hex"
 
@@ -9,11 +15,13 @@ check_env "L1_HTTP"
 check_env "L1_WS"
 check_env "L2_HTTP"
 check_env "L2_WS"
-check_env "PROVER_SET"
 check_env "TAIKO_L1"
 check_env "TAIKO_L2"
 check_env "TAIKO_TOKEN"
 check_env "L1_PROVER_PRIV_KEY"
+check_env "GUARDIAN_PROVER_MINORITY"
+check_env "GUARDIAN_PROVER_MAJORITY"
+check_env "GUARDIAN_PROVER_CONTRACT"
 
 # Run prover
 echo "Starting prover..."
