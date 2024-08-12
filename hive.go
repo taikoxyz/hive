2219b995a34c2acf6ccbba4cd9d86f479ce40ac8
maskpp <maskpp266@gmail.com>
2024-08-12 21:38:17 +0800

    fix bug

--- hive.go
+++ hive.go
@@ -23,7 +23,7 @@ func main() {
 		dockerEndpoint        = flag.String("docker.endpoint", "", "Endpoint of the local Docker daemon.")
 		dockerNoCache         = flag.String("docker.nocache", "", "Regular `expression` selecting the docker images to forcibly rebuild.")
 		dockerPull            = flag.Bool("docker.pull", false, "Refresh base images when building images.")
-		dockerOutput          = flag.Bool("docker.output", true, "Relay all docker output to stderr.")
+		dockerOutput          = flag.Bool("docker.output", false, "Relay all docker output to stderr.")
 		simPattern            = flag.String("sim", "", "Regular `expression` selecting the simulators to run.")
 		simTestPattern        = flag.String("sim.limit", "", "Regular `expression` selecting tests/suites (interpreted by simulators).")
 		simParallelism        = flag.Int("sim.parallelism", 1, "Max `number` of parallel clients/containers (interpreted by simulators).")
