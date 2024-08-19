661cbfbc0d58cff109d02836d65e6636abd50d6e
maskpp <maskpp266@gmail.com>
2024-08-19 11:53:55 +0800

    avoid /dev/null permission denied

--- simulators/taiko/scripts/get_txs.sh
+++ simulators/taiko/scripts/get_txs.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-rm -rf params/l1contract_txs.txt
+# get docker env
+. scripts/docker_env.sh
 
-L1_PROBE_URL=${L1_PROBE_URL:-http://localhost:8545}
-START_BLOCK=0
 END_BLOCK=$(cast block-number --rpc-url "$L1_PROBE_URL")
 
-for ((block = $START_BLOCK; block <= $END_BLOCK; block++)); do
+for ((block = 0; block <= "$END_BLOCK"; block++)); do
   echo "Checking block $block"
   # Get all the transactions.
   transactions=$(cast block $block --json --rpc-url "$L1_PROBE_URL" | jq -r '.transactions[]')
+
+  rm -rf params/l1contract_txs.txt
 
   # Store txs.
   for tx in $transactions; do
