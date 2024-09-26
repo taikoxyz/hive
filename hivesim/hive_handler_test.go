40457cb5ac515b09d5fe013fdb0b16da468a942f
maskpp <maskpp266@gmail.com>
2024-09-26 10:03:35 +0800

    add comments

--- hivesim/hive_handler_test.go
+++ hivesim/hive_handler_test.go
@@ -83,17 +83,19 @@ func TestTaikoGethHandler(t *testing.T) {
 	}
 
 	// Multi clusters test.
+	// ./build/bin/hive --docker.output --client anvil,taiko/taiko-geth,taiko/driver,taiko/proposer,taiko/prover,taiko/taiko-geth,taiko/driver,taiko/taiko-geth,taiko/driver --sim taiko --sim.limit "taiko-genesis/l2-snap-sync"
 	t.Run("taiko-genesis/l2-snap-sync/clusters(3)", func(t *testing.T) {
 		testDenebGenesis(t, "taiko-genesis/l2-snap-sync", clientGroups)
 	})
+	// ./build/bin/hive --docker.output --client anvil,taiko/taiko-geth,taiko/driver,taiko/proposer,taiko/prover,taiko/taiko-geth,taiko/driver,taiko/taiko-geth,taiko/driver --sim taiko --sim.limit "taiko-genesis/l2-full-sync"
 	t.Run("taiko-genesis/l2-full-sync/clusters(3)", func(t *testing.T) {
 		testDenebGenesis(t, "taiko-genesis/l2-full-sync", clientGroups)
 	})
-
+	// ./build/bin/hive --docker.output --client anvil,taiko/taiko-geth,taiko/driver,taiko/proposer,taiko/prover --sim taiko --sim.limit "taiko-reorg/taiko-reorg"
 	t.Run("taiko-reorg/taiko-reorg", func(t *testing.T) {
 		testDenebReorg(t, "taiko-reorg/taiko-reorg", [][]string{clientGroups[0]})
 	})
-
+	// ./build/bin/hive --docker.output --client geth,prysm/prysm-bn,prysm/prysm-vc,taiko/taiko-geth,taiko/driver,taiko/proposer,taiko/prover --sim taiko --sim.limit "taiko-blob/blob-l1-beacon"
 	t.Run("taiko-blob/blob-l1-beacon", func(t *testing.T) {
 		testBlobScan(t, "taiko-blob/blob-l1-beacon", []string{
 			"geth",
@@ -109,7 +111,7 @@ func TestTaikoGethHandler(t *testing.T) {
 			"blobscan/blobscan-indexer",
 		})
 	})
-
+	// ./build/bin/hive --docker.output --client geth,prysm/prysm-bn,prysm/prysm-vc,taiko/taiko-geth,taiko/driver,taiko/proposer,taiko/prover,storage/redis,storage/postgres,blobscan/blobscan-api,blobscan/blobscan-indexer --sim taiko --sim.limit "taiko-blob/blob-server"
 	t.Run("taiko-blob/blob-server", func(t *testing.T) {
 		testBlobScan(t, "taiko-blob/blob-server", []string{
 			"geth",
