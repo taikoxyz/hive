a2a1d0db5a182dbb5e6a12992469e71227d8004e
maskpp <maskpp266@gmail.com>
2024-11-12 10:22:45 +0800

    add assert functions

--- hivesim/assert.go
+++ hivesim/assert.go
@@ -1,13 +1,32 @@
 package hivesim
 
-func (t *T) Nil(err error, msg string) {
+import "fmt"
+
+func messageFromMsgAndArgs(msgAndArgs ...interface{}) string {
+	if len(msgAndArgs) == 0 || msgAndArgs == nil {
+		return ""
+	}
+	if len(msgAndArgs) == 1 {
+		msg := msgAndArgs[0]
+		if msgAsStr, ok := msg.(string); ok {
+			return msgAsStr
+		}
+		return fmt.Sprintf("%+v", msg)
+	}
+	if len(msgAndArgs) > 1 {
+		return fmt.Sprintf(msgAndArgs[0].(string), msgAndArgs[1:]...)
+	}
+	return ""
+}
+
+func (t *T) Nil(err error, msgAndArgs ...interface{}) {
 	if err != nil {
-		t.Fatalf("%s, err: %v", msg, err)
+		t.Fatalf("%s, err: %v", messageFromMsgAndArgs(msgAndArgs), err)
 	}
 }
 
-func (t *T) True(ok bool, msg string) {
+func (t *T) True(ok bool, msgAndArgs ...interface{}) {
 	if !ok {
-		t.Fatalf("%s", msg)
+		t.Fatal(messageFromMsgAndArgs(msgAndArgs))
 	}
 }
