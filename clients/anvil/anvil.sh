37f66072c55504e4c32ea160969e86eec05ab987
maskpp <maskpp266@gmail.com>
2025-03-21 14:28:18 +0800

    update pectra hardfork and remove prysm dependencies

--- clients/anvil/anvil.sh
+++ clients/anvil/anvil.sh
@@ -7,10 +7,7 @@ HIVE_LOGLEVEL=${HIVE_LOGLEVEL:-3}
 CONTAINER_IP=$(hostname -i | awk '{print $1;}')
 echo "anvil CONTAINER_IP: $CONTAINER_IP"
 
-if [ -f /hive/input/genesis.json ]; then
-  echo genesis.json:
-  cat /hive/input/genesis.json
-  anvil --init /hive/input/genesis.json --host 0.0.0.0 --accounts 20
-else
-  anvil --host 0.0.0.0 --accounts 20
-fi
+echo genesis.json:
+cat genesis.json
+
+anvil --init genesis.json --host 0.0.0.0 --accounts 20
